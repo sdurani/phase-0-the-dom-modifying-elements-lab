dc91d0d5648af83a68510dd26b04170042b67acd
Sama Durani <duranisama@gmail.com>
2024-07-14 22:55:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+document.addEventListener('DOMContentLoaded', function() {
+
+    const main = document.querySelector('main#main');
+    if (main) {
+        main.remove();
+    }
+
+
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+    newHeader.textContent = 'BOB is the champion'; 
+
+
+    document.body.appendChild(newHeader);
+});
